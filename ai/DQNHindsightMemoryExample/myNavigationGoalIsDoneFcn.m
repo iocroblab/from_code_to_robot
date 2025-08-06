dcfc6cdb0e60e0ff101ead11bf6e6caa5ed159ba
NoelDNathan <noel.nathan.planell@estudiantat.upc.edu>
2025-08-06 16:26:56 +0200

    cleaning RL path

--- ai/DQNHindsightMemoryExample/myNavigationGoalIsDoneFcn.m
+++
@@ -1,7 +0,0 @@
-function isdone = myNavigationGoalIsDoneFcn(State, Action, NextState)
-    % myNavigationGoalIsDoneFcn
-
-    % Copyright 2023 The MathWorks, Inc.
-
-    isdone = NextState{1}(2) == NextState{1}(4) && NextState{1}(3) == NextState{1}(5);
-end
