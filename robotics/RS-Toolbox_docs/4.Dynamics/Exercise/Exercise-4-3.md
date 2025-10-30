a02dd522ee703468381d3b767b34b4e449cdd956
Jan Rosell <jan.rosell@upc.edu>
2025-10-30 11:01:49 +0100

    Correctly added documentation in new location

--- robotics/RS-Toolbox_docs/4.Dynamics/Exercise/Exercise-4-3.md
+++
@@ -1,2 +0,0 @@
-
-# <span style="color:rgb(213,80,0)">Exercise 4.3 \- Track a Trajectory </span>
--- robotics/RS-Toolbox_docs/js/mathjax-config.js
+++
@@ -1,7 +0,0 @@
-window.MathJax = {
-  tex2jax: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    displayMath: [['$$', '$$'], ['\\[', '\\]']],
-    processEscapes: true
-  }
-};
